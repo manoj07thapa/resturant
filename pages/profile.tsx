9e9dea100d79b0fd3f378bcc58e6b86eae692be7
manoj07thapa <mnjinvincible@gmail.com>
2021-07-04 14:41:50 +0545

    ''

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,18 +1,9 @@
-// import { BounceLoader, ScaleLoader, BeatLoader, HashLoader } from 'react-spinners';
+import React from 'react'
 
-// export default function profile() {
-// 	return (
-// 		<div className="flex flex-col items-center mt-9 space-x-12">
-// 			<ScaleLoader size={24} color="red" loading />
-// 			<div className="mt-9">
-// 				<BounceLoader size={24} color="orange" loading />
-// 			</div>
-// 			<div className="mt-9">
-// 				<BeatLoader size={24} color="green" loading />
-// 			</div>
-// 			<div className="mt-9">
-// 				<HashLoader size={24} color="green" loading />
-// 			</div>
-// 		</div>
-// 	);
-// }
+export default function profile() {
+    return (
+        <div>
+            profile
+        </div>
+    )
+}
